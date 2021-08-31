ead0ffb6606fb7370d35ec6c56a209bba273260d
SivareddyBodapati <sivareddy3244@gmail.com>
2021-08-31 17:32:55 +0200

    updated with day 2 titanic solution

---
+++ M4-Feature Engineering/01- Gradient Boosting and Data Types/train.py
@@ -0,0 +1,5 @@
+import data_handler as dh
+
+x_train, x_test, y_train, y_test = dh.get_data(r"C:/Users/User/Documents/Strive_AI_Jun_21/M4-Feature Engineering/01- Gradient Boosting and Data Types/insurance.csv")
+
+print(dh.hello)
---
+++ M4-Feature Engineering/02 - Robust ML/trail.py
@@ -0,0 +1,3 @@
+import pandas as pd 
+df=pd.read_csv('C:/Users/User/Documents/Strive_AI_Jun_21/M4-Feature Engineering/02 - Robust ML/test.csv', index_col='PassengerId')
+print(df.shape)
